a3daaca625ec9a83c0bf6cb7d9c51182f74d0f02
Keith <klaforce@gmail.com>
2011-07-15 14:53:48 -0400

    ...

---
+++ LeisureStar/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExoGraph;
+using LeisureStar.Models;
+
+namespace LeisureStar.Controllers
+{
+	public class PlayerController : Controller
+	{
+		public ActionResult List()
+		{
+			return View();
+		}
+
+		public ActionResult Edit(string id)
+		{
+			ViewData["Player"] = GraphContext.Create<Player>(id);
+			return View();
+		}
+	}
+}
